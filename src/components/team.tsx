8fc9ff07b637f2ecc86d687a113d9dd8fc01c907
Rene Baine <renebaine13@gmail.com>
2025-10-25 11:18:30 -0400

    modified teams section

--- src/components/team.tsx
+++ src/components/team.tsx
@@ -1,64 +1,42 @@
 import { Button } from "@/components/ui/button";
-import { DribbbleIcon, TwitchIcon, TwitterIcon } from "lucide-react";
+import { Linkedin } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
 const teamMembers = [
   {
-    name: "John Doe",
+    name: "Darren Baine",
     title: "Founder & CEO",
-    bio: "Former co-founder of Opendoor. Early staff at Spotify and Clearbit.",
+    bio: "Darren Harry Baine is an Ugandan social entrepreneur, international speaker, mentor founder of Young Eye Initiative and co-founder of the IWIP App.",
     imageUrl:
-      "https://images.pexels.com/photos/3785079/pexels-photo-3785079.jpeg?auto=compress&cs=tinysrgb&w=600",
+      "",
   },
   {
-    name: "Jane Doe",
-    title: "Engineering Manager",
-    bio: "Lead engineering teams at Figma, Pitch, and Protocol Labs.",
+    name: "Felix Odong",
+    title: "Mobile App Developer",
+    bio: "",
     imageUrl:
-      "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=600",
+      "",
   },
   {
-    name: "Bob Smith",
-    title: "Product Manager",
-    bio: "Former PM for Linear, Lambda School, and On Deck.",
-    imageUrl:
-      "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=600",
+    name: "Daniel Nkurunziza",
+    title: "Web Developer",
+    bio: "Graduate of University of Kent | Computer Science with a Year in Industry",
+    imageUrl: "",
   },
   {
-    name: "Peter Johnson",
-    title: "Frontend Developer",
-    bio: "Former frontend dev for Linear, Coinbase, and Postscript.",
+    name: "Rene Baine",
+    title: "Co-Founder & Frontend Developer",
+    bio: "Bachelor of Science (Honors) in Computer Science student at Trent University",
     imageUrl:
-      "https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg?auto=compress&cs=tinysrgb&w=600",
+      "",
   },
   {
-    name: "David Lee",
-    title: "Backend Developer",
-    bio: "Lead backend dev at Clearbit. Former Clearbit and Loom.",
+    name: "Princess Mwebe",
+    title: "Social Media Manager",
+    bio: "Graduate from Northeastern University with a Bachelors of Science in Psychology in June 2024.",
     imageUrl:
-      "https://images.pexels.com/photos/2182970/pexels-photo-2182970.jpeg?auto=compress&cs=tinysrgb&w=600",
-  },
-  {
-    name: "Sarah Williams",
-    title: "Product Designer",
-    bio: "Founding design team at Figma. Former Pleo, Stripe, and Tile.",
-    imageUrl:
-      "https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&w=600",
-  },
-  {
-    name: "Michael Brown",
-    title: "UX Researcher",
-    bio: "Lead user research for Slack. Contractor for Netflix and Udacity.",
-    imageUrl:
-      "https://images.pexels.com/photos/1681010/pexels-photo-1681010.jpeg?auto=compress&cs=tinysrgb&w=600",
-  },
-  {
-    name: "Elizabeth Johnson",
-    title: "Customer Success",
-    bio: "Lead CX at Wealthsimple. Former PagerDuty and Sqreen.",
-    imageUrl:
-      "https://images.pexels.com/photos/2613260/pexels-photo-2613260.jpeg?auto=compress&cs=tinysrgb&w=600",
+      "",
   },
 ];
 
@@ -66,22 +44,12 @@ const Team = () => {
   return (
     <div className="flex flex-col justify-center py-8 sm:py-16 px-6 lg:px-8 max-w-(--breakpoint-xl) mx-auto gap-16">
       <div className="text-center max-w-2xl mx-auto">
-        <b className="text-center text-muted-foreground text-sm font-semibold uppercase">
-          We&apos;re hiring!
-        </b>
-        <h2 className="mt-3 text-4xl sm:text-5xl font-semibold tracking-tighter">
+        <h2 className="mt-3 text-4xl sm:text-5xl font-bold tracking-tighter">
           Meet Our Team
         </h2>
         <p className="mt-6 text-base sm:text-lg text-muted-foreground">
-          Our philosophy is simple — hire a team of diverse, passionate people
-          and foster a culture that empowers you to do you best work.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat ullam necessitatibus esse, voluptates cupiditate amet sed. Esse rem laudantium assumenda, unde similique ratione deleniti maiores.
         </p>
-        <div className="mt-8 flex flex-col sm:flex-row-reverse sm:justify-center gap-3">
-          <Button size="lg">Open Positions</Button>
-          <Button size="lg" variant="outline">
-            About Us
-          </Button>
-        </div>
       </div>
 
       <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-10 gap-y-12">
@@ -102,13 +70,7 @@ const Team = () => {
             <p className="mt-2 mb-6 text-pretty">{member.bio}</p>
             <div className="mt-auto flex items-center gap-4">
               <Link href="#" target="_blank">
-                <TwitterIcon className="stroke-muted-foreground h-5 w-5" />
-              </Link>
-              <Link href="#" target="_blank">
-                <DribbbleIcon className="stroke-muted-foreground h-5 w-5" />
-              </Link>
-              <Link href="#" target="_blank">
-                <TwitchIcon className="stroke-muted-foreground h-5 w-5" />
+                <Linkedin className="stroke-muted-foreground h-5 w-5" />
               </Link>
             </div>
           </div>
