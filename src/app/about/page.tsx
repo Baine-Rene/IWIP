e1ac521b6a85f1fc1c56ccf3c8f1447f5dc9cd7d
Rene Baine <renebaine13@gmail.com>
2025-10-27 14:34:17 -0400

    Changes to the about us page spacing and content

---
+++ src/app/about/page.tsx
@@ -0,0 +1,14 @@
+
+import Navbar from "@/components/navigation/navigation";
+import Footer from "@/components/footer";
+
+import { About } from "@/components/about";
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <About/>
+      <Footer />
+    </>
+  );
+}
