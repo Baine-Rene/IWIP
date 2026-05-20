327c21db8bac33d3cb02b360b55bb00650049e95
Rene Baine <renebaine04@gmail.com>
2026-05-19 20:20:34 -0400

    Added: Font awesome icons and edited font to FAQ

--- src/components/faq.tsx
+++ src/components/faq.tsx
@@ -36,8 +36,8 @@ const faq: Array<{ question: string; answer: string; listItems?: string[] }> = [
 const FAQ = () => {
   return (
     <div className="bg-primary min-h-screen flex items-center justify-center px-5 py-12">
-      <div className="w-full max-w-3xl">
-        <h2 className="text-4xl sm:text-5xl md:text-5xl leading-[1.15]! font-bold tracking-tighter text-white">
+      <div className="w-full max-w-3xl font-roboto">
+        <h2 className="text-4xl sm:text-5xl md:text-5xl leading-[1.15]! font-bold  tracking-tighter text-white">
           Frequently Asked Questions
         </h2>
         <p className="mt-2 text-xl text-muted max-w-lg">
